d6ed94057953e7c75a434ccaee0e7c4a680f4d6f
Matheus Graciano (akira) <matheusgraciano@protonmail.com>
2019-05-18 14:50:10 -0300

    more tests

--- tests/src/utils.test.js
+++ tests/src/utils.test.js
@@ -20,3 +20,36 @@ describe('random(n)', () => {
   });
   it('is <= 0', () => expect(random(1000)).toBeGreaterThanOrEqual(0));
 });
+
+// jest doesn't work with promises (yet)
+// https://stackoverflow.com/questions/51126786/jest-fake-timers-with-promises
+describe('sleep(time)', () => {
+  const time = 50;
+  it('awaits `time` milisseconds', async () => {
+    const callback = jest.fn();
+    sleep(time).then(callback);
+    expect(callback).not.toBeCalled();
+    await sleep(time);
+    expect(callback).toBeCalled();
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('never fails', async () => {
+    jest.clearAllTimers()
+    const callbackError = jest.fn();
+    sleep(time).then(jest.fn()).catch(callbackError);
+    await sleep(time);
+    expect(callbackError).not.toBeCalled();
+  });
+});
+
+describe('randomElem', () => {
+  const arr1 = [0, 1, 2, 3];
+  const arr2 = ['Michelangelo', 'Donatello', 'Leonardo', 'Rafael'];
+  it('returns a member of the array', () => {
+    expect(arr1.includes(randomElem(arr1))).toBeTrue;
+    expect(arr2.includes(randomElem(arr2))).toBeTrue;
+  })
+  it('doesn\'t return falsey elements (unless there is such an element in the array)',
+    () => expect(randomElem(arr2)).not.toBeFalsey);
+});
